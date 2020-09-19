dc7023e54e0aefe33358cacdc22a621af679670b
Nathaniel Cagle <nathanielcagle@gmail.com>
2020-09-19 16:07:12 -0700

    Was trying to open an OpenGL window until I realized WSL has no graphics

---
+++ glad.cpp
@@ -0,0 +1,9 @@
+#include <glad/glad.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Glad works!";
+
+    return 0;
+}
---
+++ glfw.cpp
@@ -0,0 +1,9 @@
+#include <GL/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "GLFW works!";
+
+    return 0;
+}
